7cffbd46e39d6945e3f986a5c6a9a856f9d8ce95
g-harel <gabrielj.harel@gmail.com>
2021-05-03 23:43:14 -0500

    proxy requests from dev server

--- common/endpoints.ts
+++ common/endpoints.ts
@@ -1,32 +1,15 @@
 import {Endpoint, DefaultClient} from "rickety";
-import {ClientRequest, ClientResponse} from "rickety/client";
 
 import {ISubmitResponse, ISubmitRequest, ICreateResponse} from "./types";
 
-const USE_DEV_SERVER = false;
-
-// Switch to dev host on localhost.
-class BaseClient extends DefaultClient {
-    async send(request: ClientRequest): Promise<ClientResponse> {
-        if (location.hostname === "localhost") {
-            if (USE_DEV_SERVER) {
-                request.url = "http://localhost:3001" + request.url;
-            } else {
-                request.url = "https://humanityagainstcards.com" + request.url;
-            }
-        }
-        return super.send(request);
-    }
-}
-
 export const CreateGame = new Endpoint<{}, ICreateResponse>({
-    client: new BaseClient(),
+    client: new DefaultClient(),
     method: "POST",
     path: "/api/v1/game",
 });
 
 export const SubmitGame = new Endpoint<ISubmitRequest, ISubmitResponse>({
-    client: new BaseClient(),
+    client: new DefaultClient(),
     method: "POST",
     path: "/api/v1/submit",
 });
--- testing/server.ts
+++ testing/server.ts
@@ -1,13 +1,33 @@
 import express from "express";
-import cors from "cors";
+import httpProxy from "http-proxy";
 
 import {createGameHandler, submitGameHandler} from "../functions/handlers";
 
 const PORT = process.env.PORT || 3000;
+const FILE_PORT = process.env.FILE_PORT || 1234;
+const REMOTE_FUNC = process.env.REMOTE_FUNC === "true";
+
+const FILE_PROXY = "http://localhost:" + FILE_PORT;
+const REMOTE_FUNC_PROXY = "http://humanityagainstcards.com";
 
 const app = express();
+const proxy = httpProxy.createProxy();
 
-app.use(cors());
+app.use((req, res, next) => {
+    if (!req.url.startsWith("/api")) {
+        console.log(`>> ${FILE_PROXY}${req.url}`);
+        proxy.web(req, res, {target: FILE_PROXY});
+        return;
+    }
+    if (REMOTE_FUNC) {
+        console.log(`>> ${REMOTE_FUNC_PROXY}${req.url}`);
+        proxy.web(req, res, {target: REMOTE_FUNC_PROXY});
+        return;
+    }
+    console.log(`   ${req.url}`);
+    next();
+});
+
 app.use(createGameHandler);
 app.use(submitGameHandler);
 
